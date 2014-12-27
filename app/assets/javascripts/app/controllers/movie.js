15bc4d855a1cbd5e97019cf51f79b1a5accbeb20
johnptmcdonald <johnptmcdonald@gmail.com>
2014-12-27 14:08:04 +0000

    added  as dependency

--- app/assets/javascripts/app/controllers/movie.js
+++ app/assets/javascripts/app/controllers/movie.js
@@ -1,19 +1,12 @@
 
-angular.module("popcornApp.controllers")
+angular.module('popcornApp.controllers')
+.controller('MovieController', ['$scope', '$routeParams', "MoviesService", "$sce",
+  function($scope, $routeParams, MoviesService, $sce) {
+    console.log($routeParams);
 
-.controller("MovieController", function($scope, MoviesService, $routeParams, $sce){
-// routeParams gives us access to the params in our function
-	console.log("MovieController is booting");
-
-	$scope.movies = MoviesService.movies();
-// the underscore.js library makes it easy for us to search through an array,
-// so we use gem 'underscore-rails'
-	$scope.movie = _.find($scope.movies, function(movie){
-		return movie.youtubeId == $routeParams.movie_id;
-	});
-// you can check out how to use _find here: http://underscorejs.org/#find
-
-// now we'll make sure the app understands that youtube stuff is a trusted resource, using $sce service
-	$scope.movie.youtubeUrl = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + $scope.movie.youtubeId + "?rel=0");
-});
+    $scope.movies = MoviesService.movies();
+    $scope.movie = _.find($scope.movies, function(v){ return v.youtubeId == $routeParams.movie_id; });
+    $scope.movie.youtubeUrl = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + $scope.movie.youtubeId + "?rel=0"); 
+  }
+]);
 
